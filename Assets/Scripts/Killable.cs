40c4835f32b5b087ccc05ff66ff36dd70dadbe52
SilviuShader <silviu.stancioiu@gmail.com>
2021-11-24 17:28:37 +0200

    Revert "Merge pull request #13 from Squid-Games/health"
    This reverts commit 92f5923822641896b960e38edc0e718a7b3d41b1, reversing
    changes made to 9639c8eb0c25b57186b2284c1c1d11c46820b467.

--- Assets/Scripts/Killable.cs
+++ Assets/Scripts/Killable.cs
@@ -1,33 +1,20 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Killable : MonoBehaviour
 {
-
-	public int life;
-	public Sprite fullHeart;
-    public Sprite emptyHeart;
-	public Image[] hearts;
-
     void Start()
     {
-		
+
     }
 
     void Update()
     {
-		for (int i = 0; i < hearts.Length; i++)
-        {
-            hearts[i].enabled = i < life;
-        }
+
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Bullets"){
-			this.life--;
-			if(this.life<=0)
-				Destroy(gameObject);
-		}
+        if (collision.gameObject.tag == "Bullets")
+            Destroy(gameObject);
     }
 }
