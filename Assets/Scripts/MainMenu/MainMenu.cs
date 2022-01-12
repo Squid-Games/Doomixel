e3aacb2d2937d769e1b06ff75737c30ccb5e8f9f
Darius Buhai <32747947+DariusBuhai@users.noreply.github.com>
2022-01-12 22:12:29 +0200

    Minor update

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,6 +5,7 @@ public class MainMenu : MonoBehaviour
     public static DataManager.SavedData savedData;
     public void Start()
     {
+        Debug.Log("Menu started");
         savedData = DataManager.Load();
     }
 }
